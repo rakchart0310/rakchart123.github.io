f43cfd3910ffeff6569997200761d5a5b1481c4c
rakchart0310 <37840220+rakchart0310@users.noreply.github.com>
2018-03-29 12:13:22 +0700

    init

---
+++ assets/js/test.js
@@ -0,0 +1,53 @@
+//current energy consumption update picture
+function update_data(id, number) {
+    $("#"+String(id)).text(String(number));
+}
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + String(Math.floor(Math.random() * 10))).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+$( document ).ready(function() {
+    console.log("starting document!!!!");
+
+     $(function ($)  {
+         $("#firebasebutton").click(function (evt) {
+           console.log("firebasebutton was clicked")
+           writeUserData('warodom', 'warodom khamphanchai',
+           'kwarodom@vt.edu', 'https://pbs.twimg.com/profile_images/966896631147765760/AJ836huS_400x400.jpg')
+         })
+     });
+
+
+    // Initialize Firebase
+    console.log("Initialize Firebase");
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA7B_WQ6e_AaIesZZCbNQyGpqx4sOopzvs",
+      authDomain: "test1-fb2c2.firebaseapp.com",
+       databaseURL: "https://test1-fb2c2.firebaseio.com",
+       projectId: "test1-fb2c2",
+       storageBucket: "test1-fb2c2.appspot.com",
+       messagingSenderId: "988611016244"
+    };
+    firebase.initializeApp(config);
+
+    var ref = firebase.database().ref();
+
+    var member_profitRef = firebase.database().ref("member");
+
+    member_profitRef.on("child_changed", function(data) {
+        console.log(data.key);
+        console.log(data.val());
+        if(data.key == "1PV221445K1200100") {
+            total_load_activePower  = data.val().load_activePower;
+        } else {
+           update_data(data.key, parseInt(data.val()))
+        }
+    });
+
+});
